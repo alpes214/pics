5d46d0486c82b5ddc96985f378207f0f31495b67
Valentin Rakush <alpes214@gmail.com>
2022-09-11 20:29:37 +0300

    submit

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,18 +1,23 @@
+import { toHaveStyle } from "@testing-library/jest-dom/dist/matchers";
 import React from "react";
 
 
 class SearchBar extends React.Component {
-    onInputChange(event) {
-        console.log(event.target.value);
+    state = {term: ''};
+
+
+    onFormSubmit = (e) => {
+        e.preventDefault();
+        console.log(this.state);
     }
 
     render () {
         return (
             <div className="ui segment">
-                <form className="ui form">
+                <form className="ui form" onSubmit={this.onFormSubmit}>
                     <div className="field">
                         <label>Search text</label>
-                        <input type="text" onChange={this.onInputChange}/>
+                        <input type="text" value={this.state.term} onChange={(e) => this.setState({term: e.target.value})}/>
                     </div>
                 </form>
             </div>
