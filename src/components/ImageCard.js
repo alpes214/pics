b97fb74b85bfdb8ab3b7cc01cb956b33792e76e7
Valentin Rakush <alpes214@gmail.com>
2022-10-17 23:14:55 +0300

    listener

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -1,12 +1,27 @@
 import React from "react";
 
 class ImageCard extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.imageRef = React.createRef();
+    }
+
+    componentDidMount() {
+        this.imageRef.current.addEventListener('load', this.setSpan);
+    }
+
+    setSpan = () => {
+        console.log(this.imageRef.current.clientHeight);
+    }
+
     render () {
+        const { description, urls } = this.props.image;
         return(
             <div>
-                <img 
-                alt={this.props.image.description}
-                src={this.props.image.urls.regular}
+                <img ref={this.imageRef}
+                    alt={description}
+                    src={urls.regular}
                 />
             </div>
         );
